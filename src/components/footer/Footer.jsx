67f0fa80028183f0fcd6abb949b60d782e0771b4
orandorisk <danjentaifib@students.amikom.ac.id>
2022-04-29 10:28:48 +0700

    menambahkan footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ import { IoLogoTwitter } from 'react-icons/io'
 const Footer = () => {
     return (
         <footer>
-            <a href="/#" className='footer__logo'>Orlando Oi</a>
+            <a href="/#" className='footer__logo'>Orlando</a>
 
             <ul className='permalinks'>
                 <li><a href="/#">Home</a></li>
